4764ef262d7a4fc722880301a4552653f27f443a
dong <1311727043@qq.com>
2020-05-07 18:20:38 +0800

    fix

--- Pods/Target Support Files/TestB/TestB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TestB : NSObject
-@end
-@implementation PodsDummy_TestB
-@end
--- Pods/Target Support Files/TestBasis/TestBasis-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TestBasis : NSObject
-@end
-@implementation PodsDummy_TestBasis
-@end
